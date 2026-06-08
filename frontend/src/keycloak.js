5965f9140d7fd75f26803117052f58b8ebe53d92
Myroslava Movchan <myroslava.mov@gmail.com>
2026-06-08 15:37:39 +0300

    Update Keycloak configuration parameters

--- frontend/src/keycloak.js
+++ frontend/src/keycloak.js
@@ -1,9 +1,9 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-  url: import.meta.env.VITE_KEYCLOAK_URL || "https://keycloak.192.168.50.10.nip.io",
-  realm: "api-security",
-  clientId: "spa-token-demo",
+  url: "https://keycloak.192.168.50.10.nip.io",
+  realm: "myroslava-movchan-lab",
+  clientId: "notes-spa",
 });
 
 export default keycloak;
